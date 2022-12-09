8f1e64865a0ab5126a468ea2bbb0e934957cabd1
Vanekchekarmum <divan.brexov@mail.ru>
2022-12-09 13:35:46 +0500

    adaptive

--- EdPlatform/EdPlatform/urls.py
+++ EdPlatform/EdPlatform/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,4 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', include('oursite.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EdPlatform/static/js/price.js
+++
@@ -1,6 +0,0 @@
-function doprice() {
-    var list = document.getElementsByClassName('price');
-    for (i=0; i<list.length; i++){
-        list[i].innerHTML = document.getElementsByClassName('price')[i].textContent.replace(',', ' ').replace(',', ' ');
-        }
-    }
